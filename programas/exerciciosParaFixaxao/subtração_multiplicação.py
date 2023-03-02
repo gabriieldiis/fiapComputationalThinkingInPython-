815ef23edb3cfa87970ce9a9757b3e1055359324
Gabriel Dias <87398682+gabriieldiis@users.noreply.github.com>
2023-03-01 21:21:36 -0300

    Exercicios
    comit dos exercicios subtração_multiplicação.py | media_aritimedica.py

---
+++ programas/exerciciosParaFixaxao/subtração_multiplicação.py
@@ -0,0 +1,23 @@
+#· Considerando 2 números inteiros, faça a soma, subtração e multiplicação. Por fim, mostre os resultados.
+print("---------------")
+print("Seja bem vindo!")
+print("---------------")
+
+numero1 = int(input("Digite seu primeiro número: "))
+numero2 = int(input("Digite seu segundo número: "))
+
+print("---------------")
+print("Seu primeiro número é: ", numero1)
+print("---------------")
+
+print("---------------")
+print("Seu segundo número é: ", numero2)
+print("---------------")
+
+subtracao = numero1 - numero2
+multiplicacao = numero1 * numero2
+
+
+print("a SUBTRAÇÃO dos números colocados é igual à = ", subtracao)
+print("a MULTIPLICACAO dos números colocados é igual à = ", multiplicacao)
+
